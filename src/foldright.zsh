a191cb51c4ec5d90bbf2ccac87036c8c4feae451
Arash Rouhani <miffoljud@gmail.com>
2012-10-07 17:44:24 -0400

    Add foldright
    This is the first method that only has a lambda version

---
+++ src/foldright.zsh
@@ -0,0 +1,31 @@
+. $(dirname $0)/fold.zsh
+
+foldright () {
+  if (($#<2)) {
+    {
+    print -- 'usage: foldright lambda list'
+    print 
+    print -- 'example:'
+    print -- '    > fold 0 1 2 3 4 5' 
+    print -- '    15'
+    } >&2
+    return 1
+  } else {
+    local body=$1
+    local acc=$2
+    shift 2
+    for i in {$#..1}
+    do
+      acc=$(foldrighte_ $@[i] $acc $body)
+    done
+    print -- $acc
+    return 0
+  }
+}
+
+foldrighte_ () {
+  local x=$1 # Indeed unlike left fold
+  local acc=$2
+  local body=$3
+  eval "${(e)body}"
+}
---
+++ tests/foldright.t
@@ -0,0 +1,10 @@
+This is right fold, in haskell notation obeying
+foldr f z [x1, x2, ..., xn] == x1 `f` (x2 `f` ... (xn `f` z)...)
+
+  $ foldright 'echo "($x <--> $acc)"' z x1 x2
+  (x1 <--> (x2 <--> z))
+
+Also acc is $2 and x is $1 now unlike fold left
+
+  $ foldright 'echo "($1 <--> $2)"' z x1 x2
+  (x1 <--> (x2 <--> z))
