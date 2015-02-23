cdcfe017fd65de0701140a079eb2c70e71d890b4
Derek Chen-Becker <derek@simpleenergy.com>
2015-02-23 09:52:49 -0700

    Refactor to use autoloading for functions
    Also made more use of explicit pathnames rather than trying to deduce
    from $0 (not always compatible with autoloading).

--- functional.plugin.zsh
+++ functional.plugin.zsh
@@ -1,6 +1,17 @@
 #!/bin/zsh
+
+ZSH_FUNCTIONAL_BASEDIR=$0:h/src
+
+fpath+=($ZSH_FUNCTIONAL_BASEDIR)
+
+# Ensure Docs are loaded
+. ${ZSH_FUNCTIONAL_BASEDIR}/docs.zsh
+
+# Load metafunction
+. ${ZSH_FUNCTIONAL_BASEDIR}/loop-magic.zsh
+
 # Load functions
-for file in $(dirname $0)/src/{map,each,filter,fold,foldright,loop-magic,docs}.zsh
+for funcname in ${ZSH_FUNCTIONAL_BASEDIR}/{each{,f},filter{,f,a},fold{,a,f,right},map{,a},param_is_{constant_if_at_all,singleton}}(:t)
 do
-  . $file
+  autoload $funcname
 done
--- src/docs.zsh
+++ src/docs.zsh
@@ -1,17 +1,3 @@
-param_is_singleton () {
-  local par=$1
-  [[ $par[1] != '[' ]]
-}
-
-param_is_constant_if_at_all () {
-  # A parameter that is constant, but can be optional (think [--] in git add)
-  local par=$1
-  ([[ $par[1] != '<' ]] &&
-    [[ $par[1] != '[' ]])
-}
-
 docsRecursionLevel=0
-metaName="$(dirname $0)/__init-$(basename $0)"
-initDocs=$(cat $metaName)
-metaName="$(dirname $0)/__done-$(basename $0)"
-doneDocs=$(cat $metaName)
+initDocs=$(<${ZSH_FUNCTIONAL_BASEDIR}/__init-docs.zsh)
+doneDocs=$(<${ZSH_FUNCTIONAL_BASEDIR}/__done-docs.zsh)
